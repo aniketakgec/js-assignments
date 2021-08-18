4938559b5100da47e5684523713e2b1cc9192497
Aniket Kumar Pandey <aniketpandey353@gmail.com>
2021-08-18 19:01:07 +0530

    task-02 updated

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -214,7 +214,7 @@ function roundToPowerOfTen(num, pow) {
  */
 function isPrime(n) {
     for (let i = 2; i <= Math.sqrt(n); i++) {
-        if (n % i == 0) return false;
+        if (n % i === 0) return false;
     }
     return true;
 }
